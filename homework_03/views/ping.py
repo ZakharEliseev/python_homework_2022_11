0fd7ee1fc0cb039308e7a1869ca680cb71668f67
ZakharEliseev <63101158+ZakharEliseev@users.noreply.github.com>
2023-01-24 12:11:11 +0500

    add all files homework_3

--- homework_03/views/ping.py
+++ homework_03/views/ping.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
-from http import HTTPStatus
+
 
 router = APIRouter(prefix='/ping')
 
 
 @router.get("/")
 async def ping():
-    m = {'status_code': HTTPStatus.OK, "message": "pong"}
-    return m
+    return {"message": "pong"}
